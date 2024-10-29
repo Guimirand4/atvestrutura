ce29c52babf4c8f9b3573440ab03eb94ae37886a
Guilherme Miranda <guilhermemiranda419@gmail.com>
2024-10-29 13:00:23 -0300

    commit

---
+++ src/grafos/TestaGrafo.java
@@ -0,0 +1,17 @@
+package grafos;
+public class TestaGrafo {
+    public static void main(String[] args) {
+        Grafo<String> grafo = new Grafo<String>();
+        grafo.adicionarVertice("João");
+        grafo.adicionarVertice("Enzo");
+        grafo.adicionarVertice("Kaik");
+        grafo.adicionarVertice("Guilherme");
+        grafo.adicionarVertice("Inacio");
+        
+        grafo.adicionarAresta(2.0, "João", "Inacio");
+        grafo.adicionarAresta(2.0, "Enzo", "Inacio");
+        grafo.adicionarAresta(2.0, "Inacio", "Kaik");
+        grafo.adicionarAresta(2.0, "Kaik", "Guilherme");
+        
+    }
+}
